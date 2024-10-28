ddf14d9b5a1c090e3b917cee3ac726a257d32b89
ycs-201607083 <nan0805@naver.com>
2024-10-28 12:21:32 +0900

    first commit

---
+++ src/main/java/com/example/prjpro2/PrjPro2Application.java
@@ -0,0 +1,13 @@
+package com.example.prjpro2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PrjPro2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PrjPro2Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/prjpro2/PrjPro2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.prjpro2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PrjPro2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
